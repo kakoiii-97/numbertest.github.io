397771d07351f093418eb2ba5145eff3ceedc98a
kakoiii3 <xutiancai97@gmail.com>
2025-08-03 18:49:00 +0800

    Add files via upload

---
+++ AI_main.py
@@ -0,0 +1,40 @@
+from keras.models import model_from_json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from PIL import Image
+import numpy as np
+import base64
+import io
+
+
+with open('handwriting_model_architecture.json', 'r') as f:
+    model_json = f.read()
+
+model = model_from_json(model_json)
+model.load_weights('handwriting_model.weights.h5')
+
+app = Flask(__name__)
+CORS(app)  # 開啟跨域權限
+
+@app.route('/AI_main', methods=['POST'])
+def handle_image():
+    data = request.get_json()
+    if not data or 'image' not in data:
+        return jsonify({'error': 'Missing image field'}), 400
+
+    img_base64 = data['image'].split(',')[1]  
+    # 去除 base64 開頭
+    img_bytes = base64.b64decode(img_base64)
+    img = Image.open(io.BytesIO(img_bytes)).convert('L')
+    img = img.resize((28, 28))
+
+    img_array = np.array(img) / 255.0    # 轉成 NumPy 陣列並正規化
+    img_array = img_array.reshape(1, 784)
+    
+    result = model.predict(img_array)
+    predicted_class = int(result.argmax())
+    return jsonify({'result': int(predicted_class)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ picture.js
@@ -0,0 +1,36 @@
+var pic=document.getElementById("picture");
+var picx = picture.getContext("2d");
+function delpic(){
+    picx.clearRect(0, 0, picture.width, picture.height);
+}
+function senttest(){
+  let base64image = pic.toDataURL("image/png");
+  fetch("http://127.0.0.1:5000/AI_main", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ image: base64image })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.result))
+}
+let drawing = false;
+
+pic.addEventListener("mousedown", (e) => {
+  drawing = true;
+  picx.beginPath();         // 開始新的繪圖路徑
+  picx.moveTo(e.offsetX, e.offsetY); // 起點
+});
+
+pic.addEventListener("mousemove", (e) => {
+  if (!drawing) return;
+  picx.lineTo(e.offsetX, e.offsetY); // 畫到目前滑鼠位置
+  picx.strokeStyle = "black";
+  picx.lineWidth = 2;
+  picx.lineJoin = "round";
+  picx.lineCap = "round";
+  picx.stroke();           // 繪出線條
+});
+
+pic.addEventListener("mouseup", () => {
+  drawing = false;
+});
